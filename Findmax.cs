49461852bd1b037a1d5bb1da9b9d08a62203e817
goya123 <akhilg642@gmail.com>
2022-07-28 17:25:26 +0530

    Find max of given three intergers

---
+++ Findmax.cs
@@ -0,0 +1,19 @@
+﻿namespace Findthemax
+{
+    public class findmax
+    {
+        public int find(int num1,int num2,int num3)
+        {
+            if ((num1.CompareTo(num2) >= 0) && (num1.CompareTo(num3) >= 0))
+                return num1;
+            else if ((num2.CompareTo(num1) >= 0) && (num2.CompareTo(num3) >= 0))
+                return num2;
+            else if ((num3.CompareTo(num2) >= 0) && (num3.CompareTo(num1) >= 0))
+                return num3;
+            else
+                return 0; 
+
+                
+        }
+    }
+}
