19f804357324fdd9a56c3f9014abdf0d8f32c6f8
goya123 <akhilg642@gmail.com>
2022-07-28 17:10:26 +0530

    Create Project with welcome message on home screen

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Findthemax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Find Max Program ");
+        }
+    }
+}
