8a2d9e27823f8b4c025cdafdccd95545c98e1c50
goya123 <akhilg642@gmail.com>
2022-07-28 17:44:33 +0530

    calling findfloat method from main

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@
         static void Main(string[] args)
         {
             int num1, num2, num3;
+            double num4, num5, num6;
             Console.WriteLine("Welcome to the Find Max Program ");
             findmax obj = new findmax();
-            Console.WriteLine("Max = "+obj.find(num1 = 10, num2 = 10, num3 = 10));
+
+            Console.WriteLine("Max = "+obj.findint(num1 = 30, num2 = 10, num3 = 20));
+            
+            Console.WriteLine("Max float = "+obj.findfloat(num4 = 10.5, num5 = 10.6, num6 = 10.7));
+
 
         }
     }
