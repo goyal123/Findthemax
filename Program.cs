b83119847794c9fc53d9045c3dcb3c4115f4c6eb
goya123 <akhilg642@gmail.com>
2022-07-28 17:26:51 +0530

    Add code to comunicate with Findthemax class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+            int num1, num2, num3;
             Console.WriteLine("Welcome to the Find Max Program ");
+            findmax obj = new findmax();
+            Console.WriteLine("Max = "+obj.find(num1 = 10, num2 = 10, num3 = 10));
+
         }
     }
 }
