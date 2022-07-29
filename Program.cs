f873d9bf6c34cd99d7cb6efafb8f3f382481cfa9
goya123 <akhilg642@gmail.com>
2022-07-29 18:20:30 +0530

    Modifying code to implement gerneric class functionality

--- Program.cs
+++ Program.cs
@@ -7,14 +7,23 @@
             int num1, num2, num3;
             double num4, num5, num6;
             string num8, num9, num10;
+
             Console.WriteLine("Welcome to the Find Max Program ");
-            findmax obj = new findmax();
+            Console.WriteLine("-----------------------------------------");
 
-            Console.WriteLine("Max = "+obj.findint(num1 = 30, num2 = 10, num3 = 20));
-            
-            Console.WriteLine("Max float = "+obj.findfloat(num4 = 10.5, num5 = 10.6, num6 = 10.7));
+            Findmax<int> obj1 = new Findmax<int>(num1 = 30, num2 = 10, num3 = 20);
+            obj1.TestMaximum();
+            Findmax<double> obj2 = new Findmax<double>(num4 = 10.5, num5 = 10.6, num6 = 10.7);
+            obj2.TestMaximum();
+            Findmax<string> obj3 = new Findmax<string>(num8 = "Apple", num9 = "Peach", num10 = "Banana");
+            obj3.TestMaximum();
 
-            Console.WriteLine("Max string = " + obj.findstr(num8 = "Apple", num9 = "Peach", num10 = "Banana"));
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Max = " + Findmax<int>.Findingmax(30,10,20));
+            Console.WriteLine("Max = " + Findmax<double>.Findingmax(10.5, 10.6, 10.7));
+            Console.WriteLine("Max = " + Findmax<string>.Findingmax("Apple","Banana","Peach"));
         }
     }
 }
